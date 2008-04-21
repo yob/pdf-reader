2c3df8270dbd58a34deb1a0d208cb9e56c2b3e3c
jhealy <jhealy@3db89d08-1e44-db11-a2a8-0014220fd31b>
2008-04-21 11:30:01 +0000

    pdf-reader: Improved our Zlib deflating to make it more slightly more robust
    git-svn-id: https://pmade.com/svn/oss/pdf-reader/trunk@1384 3db89d08-1e44-db11-a2a8-0014220fd31b

--- lib/pdf/reader/filter.rb
+++ lib/pdf/reader/filter.rb
@@ -56,11 +56,19 @@ class PDF::Reader
     # Decode the specified data with the Zlib compression algorithm
     def flate (data)
       begin
-        z = Zlib::Inflate.new
-        z.inflate(data)
-      rescue Exception => e
-        raise MalformedPDFError, "Error occured while inflating a compressed stream (#{e.class.to_s}: #{e.to_s})"
+        Zlib::Inflate.new.inflate(data)
+      rescue Zlib::DataError => e
+        # by default, Ruby's Zlib assumes the data it's inflating
+        # is RFC1951 deflated data, wrapped in a RFC1951 zlib container.
+        # If that fails, then use an undocumented 'feature' to attempt to inflate
+        # the data as a raw RFC1951 stream.
+        #
+        # See http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/243545
+        Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(data)
       end
+    rescue Exception => e
+      # Oops, there was a problem inflating the stream
+      raise MalformedPDFError, "Error occured while inflating a compressed stream (#{e.class.to_s}: #{e.to_s})"
     end
     ################################################################################
   end
