7a660264b953f14234a6956cb8686af6d309d78a
James Healy <jimmy@deefa.com>
2009-11-20 01:40:43 +1100

    documentation update, mainly for PDF::Hash

--- lib/pdf/hash.rb
+++ lib/pdf/hash.rb
@@ -14,12 +14,24 @@ module PDF
   # the Enumerable mixin. The key difference is no []= method - the hash
   # is read only.
   #
+  # == Basic Usage
+  #
+  #     h = PDF::Hash.new("somefile.pdf")
+  #     h[1]
+  #     => 3469
+  #
+  #     h[PDF::Reader::Reference.new(1,0)]
+  #     => 3469
+  #
   class Hash
     include Enumerable
 
     attr_accessor :default
     attr_reader :trailer
 
+    # Creates a new PDF:Hash object. input can be a string with a valid filename,
+    # a string containing a PDF file, or an IO object.
+    #
     def initialize(input)
       if input.kind_of?(IO) || input.kind_of?(StringIO)
         io = input
@@ -38,6 +50,15 @@ module PDF
       @trailer = @xref.load
     end
 
+    # Access an object from the PDF. key can be an int or a PDF::Reader::Reference
+    # object.
+    #
+    # If an int is used, the object with that ID and a generation number of 0 will
+    # be returned.
+    #
+    # If a PDF::Reader::Reference object is used the exact ID and generation number
+    # can be specified.
+    #
     def [](key)
       return default if key.to_i <= 0
 
@@ -51,6 +72,18 @@ module PDF
       end
     end
 
+    # Access an object from the PDF. key can be an int or a PDF::Reader::Reference
+    # object.
+    #
+    # If an int is used, the object with that ID and a generation number of 0 will
+    # be returned.
+    #
+    # If a PDF::Reader::Reference object is used the exact ID and generation number
+    # can be specified.
+    #
+    # local_deault is the object that will be returned if the requested key doesn't
+    # exist.
+    #
     def fetch(key, local_default = nil)
       obj = self[key]
       if obj
@@ -62,6 +95,8 @@ module PDF
       end
     end
 
+    # iterate over each key, value. Just like a ruby hash.
+    #
     def each(&block)
       @xref.each do |ref, obj|
         yield ref, obj
@@ -69,29 +104,40 @@ module PDF
     end
     alias :each_pair :each
 
+    # iterate over each key. Just like a ruby hash.
+    #
     def each_key(&block)
       each do |id, obj|
         yield id
       end
     end
 
+    # iterate over each value. Just like a ruby hash.
+    #
     def each_value(&block)
       each do |id, obj|
         yield obj
       end
     end
 
+    # return the number of objects in the file. An object with multiple generations
+    # is counted once.
     def size
       @xref.size
     end
     alias :length :size
 
+    # return true if there are no objects in this file
+    #
     def empty?
       size == 0 ? true : false
     end
 
-    # TODO update from O(n) to O(1)
+    # return true if the specified key exists in the file. key
+    # can be an int or a PDF::Reader::Reference
+    #
     def has_key?(check_key)
+      # TODO update from O(n) to O(1)
       each_key do |key|
         if check_key.kind_of?(PDF::Reader::Reference)
           return true if check_key == key
@@ -105,8 +151,10 @@ module PDF
     alias :key? :has_key?
     alias :member? :has_key?
 
-    # TODO update from O(n) to O(1)
+    # return true if the specifiedvalue exists in the file
+    #
     def has_value?(value)
+      # TODO update from O(n) to O(1)
       each_value do |obj|
         return true if obj == value
       end
@@ -118,22 +166,30 @@ module PDF
       "<PDF::Hash size: #{self.size}>"
     end
 
+    # return an array of all keys in the file
+    #
     def keys
       ret = []
       each_key { |k| ret << k }
       ret
     end
 
+    # return an array of all values in the file
+    #
     def values
       ret = []
       each_value { |v| ret << v }
       ret
     end
 
+    # return an array of all values from the specified keys
+    #
     def values_at(*ids)
       ids.map { |id| self[id] }
     end
 
+    # return an array of arrays. Each sub array contains a key/value pair.
+    #
     def to_a
       ret = []
       each do |id, obj|
