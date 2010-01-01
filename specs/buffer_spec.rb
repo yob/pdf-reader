f7ca803fd729b0a8c2b98693f53878cdfb392cc7
James Healy <jimmy@deefa.com>
2010-01-01 16:37:05 +1100

    move buffer spec

--- specs/buffer_spec.rb
+++ specs/buffer_spec.rb
@@ -1,169 +1,90 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
+require File.dirname(__FILE__) + "/spec_helper"
 
-require 'pdf/reader'
-
-module BufferHelper
-  def parse_string (r)
-    PDF::Reader::Buffer.new(sio = StringIO.new(r))
-  end
-end
-
-context "The PDF::Reader::Buffer class when operating on the cairo-basic PDF" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-  
-  specify "should find the first xref offset correctly" do
-    @buffer.find_first_xref_offset.should eql(9243)
-  end
-
-  specify "should be able to read the PDF header correctly" do
-    @buffer.seek(0).read(8).should eql("%PDF-1.4")
-  end
-
-end
-
-context "The PDF::Reader::Buffer class when operating on the cairo-unicode PDF" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/cairo-unicode.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-
-  specify "should find the first xref offset correctly" do
-    @buffer.find_first_xref_offset.should eql(136174)
-  end
-
-  specify "should be able to read the PDF header correctly" do
-    @buffer.seek(0).read(8).should eql("%PDF-1.4")
-  end
-
-end
-
-context "The PDF::Reader::Buffer class when operating on the prince1 PDF" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/prince1.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-
-  specify "should find the first xref offset correctly" do
-    @buffer.find_first_xref_offset.should eql(678715)
-  end
-
-  specify "should be able to read the PDF header correctly" do
-    @buffer.seek(0).read(8).should eql("%PDF-1.4")
-  end
-
-end
-
-context "The PDF::Reader::Buffer class when operating on the prince2 PDF" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/prince2.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-
-  specify "should find the first xref offset correctly" do
-    @buffer.find_first_xref_offset.should eql(941440)
-  end
-
-  specify "should be able to read the PDF header correctly" do
-    @buffer.seek(0).read(8).should eql("%PDF-1.4")
-  end
-
-end
-
-context "The PDF::Reader::Buffer class when operating on the pdfwriter manual" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/pdfwriter-manual.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-
-  specify "should find the first xref offset correctly" do
-    @buffer.find_first_xref_offset.should eql(275320)
-  end
-
-  specify "should be able to read the PDF header correctly" do
-    @buffer.seek(0).read(8).should eql("%PDF-1.3")
-  end
-
-end
-
-context "The PDF::Reader::Buffer class when operating on the pdf-distiller PDF" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/pdf-distiller.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-
-  # this file uses just a \r as an EOL marker, so check we handle it properly
-  specify "should find the first xref offset correctly" do
-    @buffer.find_first_xref_offset.should eql(173)
-  end
-
-  specify "should be able to read the PDF header correctly" do
-    @buffer.seek(0).read(8).should eql("%PDF-1.2")
-  end
-
-end
-
-context "The PDF::Reader::Buffer class when operating on the pdflatex PDF" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/pdflatex.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-
-  specify "should find the first xref offset correctly" do
-    @buffer.find_first_xref_offset.should eql(152898)
-  end
-
-  specify "should be able to read the PDF header correctly" do
-    @buffer.seek(0).read(8).should eql("%PDF-1.4")
-  end
-end
-
-context "The PDF::Reader::Buffer class when operating on the openoffice-2.2 PDF" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/openoffice-2.2.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-
-  specify "should find the first xref offset correctly" do
-    @buffer.find_first_xref_offset.should eql(36961)
-  end
-
-  specify "should be able to read the PDF header correctly" do
-    @buffer.seek(0).read(8).should eql("%PDF-1.4")
-  end
-
-  specify "should be able to read all bytes up to 1.4 and leave the cursor in the right location" do
-    @buffer.seek(0).read_until("1.4").should eql("%PDF-")
-    @buffer.pos.should eql(5)
-  end
-
-end
-
-context "The PDF::Reader::Buffer class when operating on a PDF with no EOF marker" do
-
-  before do
-    @file = File.new(File.dirname(__FILE__) + "/data/invalid/no_eof.pdf")
-    @buffer = PDF::Reader::Buffer.new(@file)
-  end
-
-  specify "should raise an exception when trying to find the xref offset" do
-    lambda { @buffer.find_first_xref_offset }.should raise_error(PDF::Reader::MalformedPDFError)
-  end
-
-end
-
-context "The PDF::Reader::Buffer class" do
+context PDF::Reader::Buffer, "token method" do
   include BufferHelper
+
+  specify "should return nil when there's no IO left" do
+    buf = parse_string("aaa")
+
+    buf.token
+    buf.token.should be_nil
+  end
+
+  specify "should correctly return a simple token - 1" do
+    buf = parse_string("aaa")
+
+    buf.token.should eql("aaa")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly return a simple token - 2" do
+    buf = parse_string("1.0")
+
+    buf.token.should eql("1.0")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly return two simple tokens" do
+    buf = parse_string("aaa 1.0")
+
+    buf.token.should eql("aaa")
+    buf.token.should eql("1.0")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly tokenise opening delimiters" do
+    buf = parse_string("<[{/%(")
+
+    buf.token.should eql("<")
+    buf.token.should eql("[")
+    buf.token.should eql("{")
+    buf.token.should eql("/")
+    buf.token.should eql("%")
+    buf.token.should eql("(")
+    buf.token.should eql(")") # auto adds closing literal string delim
+    buf.token.should be_nil
+  end
+
+  specify "should correctly tokenise closing delimiters" do
+    buf = parse_string(")>]}")
+
+    buf.token.should eql(")")
+    buf.token.should eql(">")
+    buf.token.should eql("]")
+    buf.token.should eql("}")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly tokenise hash delimiters" do
+    buf = parse_string("<<aaa>>")
+
+    buf.token.should eql("<<")
+    buf.token.should eql("aaa")
+    buf.token.should eql(">>")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly return simple tokens with delimiters" do
+    buf = parse_string("<aaa><bbb>")
+
+    buf.token.should eql("<")
+    buf.token.should eql("aaa")
+    buf.token.should eql(">")
+    buf.token.should eql("<")
+    buf.token.should eql("bbb")
+    buf.token.should eql(">")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly return two name tokens" do
+    buf = parse_string("/Type/Pages")
+
+    buf.token.should eql("/")
+    buf.token.should eql("Type")
+    buf.token.should eql("/")
+    buf.token.should eql("Pages")
+    buf.token.should be_nil
+  end
 
   specify "should tokenise a dict correctly" do
     buf = parse_string("/Registry (Adobe) /Ordering (Japan1) /Supplement")
@@ -201,5 +122,169 @@ context "The PDF::Reader::Buffer class" do
     buf.token.should eql("James%Healy")
     buf.token.should eql(")")
     buf.token.should eql("(")
+  end
+  
+  specify "should correctly return an indirect reference" do
+    buf = parse_string("aaa 1 0 R bbb")
+
+    buf.token.should eql("aaa")
+    buf.token.should be_a_kind_of(PDF::Reader::Reference)
+    buf.token.should eql("bbb")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly seek to a particular byte of the IO - 1" do
+    str = "aaa bbb ccc"
+    buf = PDF::Reader::Buffer.new(StringIO.new(str), :seek => 4)
+
+    buf.token.should eql("bbb")
+    buf.token.should eql("ccc")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly seek to a particular byte of the IO - 2" do
+    str = "aaa bbb ccc"
+    buf = PDF::Reader::Buffer.new(StringIO.new(str), :seek => 5)
+
+    buf.token.should eql("bb")
+    buf.token.should eql("ccc")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly return a simple literal string" do
+    buf = parse_string("(aaa)")
+
+    buf.token.should eql("(")
+    buf.token.should eql("aaa")
+    buf.token.should eql(")")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly return a simple literal string with spaces" do
+    buf = parse_string("(aaa bbb)")
+
+    buf.token.should eql("(")
+    buf.token.should eql("aaa bbb")
+    buf.token.should eql(")")
+    buf.token.should be_nil
+  end
+
+  specify "should correctly return a simple literal string with nested brackets" do
+    buf = parse_string("(aaa (bbb))")
+
+    buf.token.should eql("(")
+    buf.token.should eql("aaa (bbb)")
+    buf.token.should eql(")")
+    buf.token.should be_nil
+  end
+end
+
+context PDF::Reader::Buffer, "empty? method" do
+  include BufferHelper
+
+  specify "should correctly return false if there are remaining tokens" do
+    buf = parse_string("aaa")
+
+    buf.empty?.should be_false
+  end
+
+  specify "should correctly return true if there are no remaining tokens" do
+    buf = parse_string("aaa")
+
+    buf.empty?.should be_false
+    buf.token
+    buf.empty?.should be_true
+  end
+end
+
+context PDF::Reader::Buffer, "find_first_xref_offset method" do
+
+  specify "should find the first xref offset from cairo-basic.pdf correctly" do
+    @file = File.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    @buffer.find_first_xref_offset.should eql(9243)
+  end
+
+  specify "should find the first xref offset from cairo-unicode.pdf correctly" do
+    @file = File.new(File.dirname(__FILE__) + "/data/cairo-unicode.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    @buffer.find_first_xref_offset.should eql(136174)
+  end
+
+  specify "should find the first xref offset from prince1.pdf correctly" do
+    @file = File.new(File.dirname(__FILE__) + "/data/prince1.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    @buffer.find_first_xref_offset.should eql(678715)
+  end
+
+  specify "should find the first xref offset from prince2.pdf correctly" do
+    @file = File.new(File.dirname(__FILE__) + "/data/prince2.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    @buffer.find_first_xref_offset.should eql(941440)
+  end
+
+  specify "should find the first xref offset from pdfwriter-manual.pdf correctly" do
+    @file = File.new(File.dirname(__FILE__) + "/data/pdfwriter-manual.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    @buffer.find_first_xref_offset.should eql(275320)
+  end
+
+  specify "should find the first xref offset from pdf-distiller.pdf correctly" do
+    @file = File.new(File.dirname(__FILE__) + "/data/pdf-distiller.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    @buffer.find_first_xref_offset.should eql(173)
+  end
+
+  specify "should find the first xref offset from pdflatex.pdf correctly" do
+    @file = File.new(File.dirname(__FILE__) + "/data/pdflatex.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    @buffer.find_first_xref_offset.should eql(152898)
+  end
+
+  specify "should find the first xref offset from openoffice-2.2.pdf correctly" do
+    @file = File.new(File.dirname(__FILE__) + "/data/openoffice-2.2.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    @buffer.find_first_xref_offset.should eql(36961)
+  end
+
+  specify "should raise an exception when buffer doesn't contain an EOF marker" do
+    @file = File.new(File.dirname(__FILE__) + "/data/invalid/no_eof.pdf")
+    @buffer = PDF::Reader::Buffer.new(@file)
+
+    lambda { @buffer.find_first_xref_offset }.should raise_error(PDF::Reader::MalformedPDFError)
+  end
+
+end
+
+context PDF::Reader::Buffer, "read method" do
+  include BufferHelper
+
+  specify "should return raw data from the underlying IO" do
+    buf = parse_string("stream bbb")
+
+    buf.token.should eql("stream")
+    buf.read(3).should eql("bbb")
+  end
+
+  specify "should return raw data from the underlying IO" do
+    buf = parse_string("stream\n\nbbb")
+
+    buf.token.should eql("stream")
+    buf.read(4).should eql("\nbbb")
+  end
+
+  specify "should return raw data from the underlying IO and skip LF/CR bytes" do
+    buf = parse_string("stream\n\nbbb")
+
+    buf.token.should eql("stream")
+    buf.read(3, :skip_eol => true).should eql("bbb")
   end
 end
