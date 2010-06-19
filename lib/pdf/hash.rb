8d3e7d9f10caee63ac32f2fa072f97ecc44560af
James Healy <jimmy@deefa.com>
2010-06-19 18:16:05 +1000

    Restore PDF::Hash#pdf_version
    * with a deprecation warning

--- lib/pdf/hash.rb
+++ lib/pdf/hash.rb
@@ -6,5 +6,10 @@ module PDF
       warn "DEPRECATION NOTICE: PDF::Hash has been deprecated, use PDF::Reader::ObjectHash instead"
       super
     end
+
+    def version
+      warn "DEPRECATION NOTICE: PDF::Hash#version has been deprecated, use PDF::Reader::ObjectHash#pdf_version instead"
+      pdf_version
+    end
   end
 end
