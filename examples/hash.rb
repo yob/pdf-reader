15c7fd2fe13fc1a0a5e390a0101441e944724cfb
James Healy <jimmy@deefa.com>
2010-06-19 18:30:50 +1000

    update an example

--- examples/hash.rb
+++ examples/hash.rb
@@ -8,5 +8,5 @@ $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
 require 'pdf/reader'
 
 filename = File.dirname(__FILE__) + "/../specs/data/cairo-unicode.pdf"
-hash = PDF::Hash.new(filename)
+hash = PDF::Reader::ObjectHash.new(filename)
 puts hash[3]
