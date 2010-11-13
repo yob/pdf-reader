62c1914c7663c44c8e783836535b9f7e349295f5
James Healy <jimmy@deefa.com>
2010-11-13 08:11:14 -0600

    LZW#decode expects a string

--- specs/lzw_spec.rb
+++ specs/lzw_spec.rb
@@ -7,7 +7,7 @@ context PDF::Reader::LZW do
   it "should correctly decode a lzw compress string" do
     content = %w{ 80 0B 60 50 22 0C 0C 85 01 }.map { |byte|
       byte.to_i(16)
-    }
+    }.pack("C*")
 
     PDF::Reader::LZW.decode(content).should == '-----A---B'
   end
