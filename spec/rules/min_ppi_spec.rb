e8ef0c82dd89c5f23a08e81d4d145ae9141c7591
James Healy <jimmy@deefa.com>
2011-03-27 22:43:37 +1100

    spec MinPPi rule

---
+++ spec/rules/min_ppi_spec.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::MinPpi do
+
+  it "pass files with a no raster images" do
+    filename  = pdf_spec_file("pdfx-1a-subsetting")
+    receiver = Preflight::Rules::MinPpi.new(300)
+    PDF::Reader.file(filename, receiver)
+
+    receiver.messages.should be_empty
+  end
+
+  it "pass files with only 300 ppi raster images" do
+    filename  = pdf_spec_file("300ppi")
+    receiver = Preflight::Rules::MinPpi.new(300)
+    PDF::Reader.file(filename, receiver)
+
+    receiver.messages.should be_empty
+  end
+
+  it "fail files with a 75ppi raster image" do
+    filename  = pdf_spec_file("72ppi")
+    receiver = Preflight::Rules::MinPpi.new(300)
+    PDF::Reader.file(filename, receiver)
+
+    receiver.messages.should_not be_empty
+  end
+
+end
