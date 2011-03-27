4c4cd99666fa825266701e45a498a5810ad9bcd0
James Healy <jimmy@deefa.com>
2011-03-27 23:07:57 +1100

    add pending specs

---
+++ spec/rules/no_proprietary_fonts_spec.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::NoFontSubsets do
+
+  it "fail files with an embedded font TTF font that disallows editing"
+
+  it "pass files with an embedded font TTF font that allows editing"
+
+end
