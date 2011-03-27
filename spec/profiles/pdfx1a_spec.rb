b70c0a9ec93eface9a50d2a593eb9851d1b8fc06
James Healy <jimmy@deefa.com>
2011-03-27 21:29:08 +1100

    add baseline specs to ensure PDF/X-1a profile continues to pass valid files

--- spec/profiles/pdfx1a_spec.rb
+++ spec/profiles/pdfx1a_spec.rb
@@ -2,6 +2,22 @@ require File.dirname(__FILE__) + "/../spec_helper"
 
 describe Preflight::Profiles::PDFX1A do
 
+  it "correctly pass a valid PDF/X-1a file that uses font subsetting" do
+    filename  = pdf_spec_file("pdfx-1a-subsetting")
+    preflight = Preflight::Profiles::PDFX1A.new
+    messages  = preflight.check(filename)
+
+    messages.empty?.should be_true
+  end
+
+  it "correctly pass a valid PDF/X-1a file that doesn't use font subsetting" do
+    filename  = pdf_spec_file("pdfx-1a-no-subsetting")
+    preflight = Preflight::Profiles::PDFX1A.new
+    messages  = preflight.check(filename)
+
+    messages.empty?.should be_true
+  end
+
   it "correctly detect files with an incompatible version" do
     filename  = pdf_spec_file("version_1_4")
     preflight = Preflight::Profiles::PDFX1A.new
