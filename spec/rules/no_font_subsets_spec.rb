6094f5e5d7c0817e210544c92563de99b94cadc9
James Healy <jimmy@deefa.com>
2011-03-27 22:56:11 +1100

    spec NoFontSubsets rule
    * need more sample files to expand spec coverage to non TTF fonts
    * in particular, I need sample files with a composite (Type0) font, I'm
      fairly sure current rule will incorrectly fail an non-subsetted Type0
      font

---
+++ spec/rules/no_font_subsets_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::NoFontSubsets do
+
+  it "fail files with a subsetted TTF font" do
+    filename = pdf_spec_file("pdfx-1a-subsetting")
+    ohash    = PDF::Reader::ObjectHash.new(filename)
+    chk      = Preflight::Rules::NoFontSubsets.new
+
+    chk.messages(ohash).should_not be_empty
+  end
+
+  it "pass files with a complete TTF font" do
+    filename = pdf_spec_file("pdfx-1a-no-subsetting")
+    ohash    = PDF::Reader::ObjectHash.new(filename)
+    chk      = Preflight::Rules::NoFontSubsets.new
+
+    chk.messages(ohash).should be_empty
+  end
+
+end
