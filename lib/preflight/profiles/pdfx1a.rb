6a3e264ae0c250b42c6a59353f784f2328c400c7
James Healy <jimmy@deefa.com>
2011-04-07 18:03:13 +1000

    new pdfx rule: no filespecs

--- lib/preflight/profiles/pdfx1a.rb
+++ lib/preflight/profiles/pdfx1a.rb
@@ -15,6 +15,7 @@ module Preflight
       rule Preflight::Rules::CompressionAlgorithms, :CCITTFaxDecode, :DCTDecode, :FlateDecode, :RunLengthDecode
       rule Preflight::Rules::DocumentId
       rule Preflight::Rules::NoEncryption
+      rule Preflight::Rules::NoFilespecs
       rule Preflight::Rules::OnlyEmbeddedFonts
       rule Preflight::Rules::BoxNesting
       rule Preflight::Rules::MaxVersion, 1.4
---
+++ lib/preflight/rules/no_filespecs.rb
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+module Preflight
+  module Rules
+
+    # PDF/X files are not allowed to use Filespecs to refer
+    # to external files.
+    #
+    class NoFilespecs
+
+      def messages(ohash)
+        if count_filespec_dicts(ohash) > 0
+          ["File uses at least 1 Filespec to refer to an external file"]
+        else
+          []
+        end
+      end
+
+      private
+
+      def count_filespec_dicts(ohash)
+        ohash.select { |key, obj|
+          obj.is_a?(::Hash) && (obj[:Type] == :Filespec || obj[:Type] == :F)
+        }.size
+      end
+    end
+  end
+end
