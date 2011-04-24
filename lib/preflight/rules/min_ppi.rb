5d163215e60babd398dd4da9aed1e207fe0579f3
James Healy <jimmy@deefa.com>
2011-04-25 00:10:25 +1000

    update MinPPI to honour the graphics state stack
    * Keeps track of the current transform matrix within the graphics state stack.
    * ignores other elements of the graphics state, they're not needed for
      this rule
    * each call to the concantenate_matrix operator needs to merge the new
      matrix with the current matrix. It's cumulative, so the previous
      tactic of only tracking the last values passed to concantenate_matrix
      was totally bogus. It worked in many cases, but only by dumb luck

--- lib/preflight/rules/min_ppi.rb
+++ lib/preflight/rules/min_ppi.rb
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+require 'yaml'
+
 module Preflight
   module Rules
 
@@ -14,8 +16,19 @@ module Preflight
       def initialize(min_ppi)
         @min_ppi = min_ppi.to_i
         @messages = []
-        @last_matrix = []
         @page_num = 0
+      end
+
+      def save_graphics_state
+        @stack.push clone_state
+      end
+
+      def restore_graphics_state
+        @stack.pop
+      end
+
+      def state
+        @stack.last
       end
 
       # store sample width and height for each image on the current page
@@ -29,12 +42,19 @@ module Preflight
         ]
       end
 
-      # track the most recent matrix transform.
+      # update the current transform matrix.
       #
-      # TODO: This needs to be smarter at tracking the graphics state stack
+      # If the CTM is currently undefined, just store the new values.
+      #
+      # If there's an existing CTM, then multiple the existing matrix
+      # with the new matrix to form the updated matrix.
       #
       def concatenate_matrix(*args)
-        @last_matrix = args
+        if state[:ctm]
+          state[:ctm] = multiply_matrix(state[:ctm], args)
+        else
+          state[:ctm] = args
+        end
       end
 
       # As each image is drawn on the canvas, determine the amount of device
@@ -44,8 +64,8 @@ module Preflight
         return unless @images[label]
 
         sample_w, sample_h = *@images[label]
-        device_w = pt2in(@last_matrix[0])
-        device_h = pt2in(@last_matrix[3])
+        device_w = pt2in(state[:ctm][0])
+        device_h = pt2in(state[:ctm][3])
 
         horizontal_ppi = (sample_w / device_w).round(3)
         vertical_ppi   = (sample_h / device_h).round(3)
@@ -60,6 +80,44 @@ module Preflight
       def begin_page(hash = {})
         @images = {}
         @page_num += 1
+        @stack = []
+      end
+
+      private
+
+      # when save_graphics_state is called, we need to push a new copy of the
+      # current state onto the stack. That way any modifications to the state
+      # will be undone once restore_graphics_state is called.
+      #
+      # This returns a deep clone of the current state, ensuring changes are
+      # keep separate from earlier states.
+      #
+      # YAML is used to round-trip the state through a string to easily perform
+      # the deep clone. Kinda hacky, but effective.
+      #
+      def clone_state
+        if @stack.empty?
+          {}
+        else
+          yaml_state = YAML.dump(@stack.last)
+          YAML.load(yaml_state)
+        end
+      end
+
+      # multiplies two transform matrixes together.
+      #
+      def multiply_matrix(current, transform)
+        raise ArgumentError, 'current matrix must have 6 elements' if current.size !=6
+        raise ArgumentError, 'transform matrix must have 6 elements' if transform.size !=6
+
+        [
+          current[0] * transform[0],
+          current[1] * transform[1],
+          current[2] * transform[2],
+          current[3] * transform[3],
+          current[4] * transform[4],
+          current[5] * transform[5]
+        ]
       end
     end
   end
