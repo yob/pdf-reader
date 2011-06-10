83c3f45b633ad7946cdd37da15d3bf6c8f85b416
James Healy <jimmy@deefa.com>
2011-06-10 23:55:43 +1000

    DRY Browser tests

--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -5,19 +5,20 @@ require File.dirname(__FILE__) + "/spec_helper"
 
 describe PDF::Reader::Browser, "with cairo-basic.pdf" do
 
+  before(:each) do
+    @browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
+  end
+
   it "should return the correct pdf_version" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
-    browser.pdf_version.should eql(1.4)
+    @browser.pdf_version.should eql(1.4)
   end
 
   it "should return the correct page_count" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
-    browser.page_count.should eql(2)
+    @browser.page_count.should eql(2)
   end
 
   it "should return the correct info hash" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
-    info    = browser.info
+    info    = @browser.info
 
     info.size.should eql(2)
     info[:Creator].should eql("cairo 1.4.6 (http://cairographics.org)")
@@ -25,62 +26,57 @@ describe PDF::Reader::Browser, "with cairo-basic.pdf" do
   end
 
   it "should return the nil for metadata" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
-    browser.metadata.should be_nil
+    @browser.metadata.should be_nil
   end
 
   it "should return an array of BrowserPages from pages()" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
-    browser.pages.should be_a_kind_of(Array)
-    browser.pages.size.should eql(browser.page_count)
-    browser.pages.each do |page|
+    @browser.pages.should be_a_kind_of(Array)
+    @browser.pages.size.should eql(@browser.page_count)
+    @browser.pages.each do |page|
       page.should be_a_kind_of(PDF::Reader::BrowserPage)
     end
   end
 
   it "should return a single BrowserPage from page()" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
-    browser.page(1).should be_a_kind_of(PDF::Reader::BrowserPage)
+    @browser.page(1).should be_a_kind_of(PDF::Reader::BrowserPage)
   end
 
 end
 
 describe PDF::Reader::Browser, "with no_text_spaces.pdf" do
 
+  before(:each) do
+    @browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/no_text_spaces.pdf")
+  end
+
   it "should return the correct pdf_version" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/no_text_spaces.pdf")
-    browser.pdf_version.should eql(1.4)
+    @browser.pdf_version.should eql(1.4)
   end
 
   it "should return the correct page_count" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/no_text_spaces.pdf")
-    browser.page_count.should eql(6)
+    @browser.page_count.should eql(6)
   end
 
   it "should return the correct info hash" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/no_text_spaces.pdf")
-    info    = browser.info
+    info    = @browser.info
 
     info.size.should eql(9)
   end
 
   it "should return the nil for metadata" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/no_text_spaces.pdf")
-    browser.metadata.should be_a_kind_of(String)
-    browser.metadata.should include("<x:xmpmeta")
+    @browser.metadata.should be_a_kind_of(String)
+    @browser.metadata.should include("<x:xmpmeta")
   end
 
   it "should return an array of BrowserPages from pages()" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/no_text_spaces.pdf")
-    browser.pages.should be_a_kind_of(Array)
-    browser.pages.size.should eql(browser.page_count)
-    browser.pages.each do |page|
+    @browser.pages.should be_a_kind_of(Array)
+    @browser.pages.size.should eql(@browser.page_count)
+    @browser.pages.each do |page|
       page.should be_a_kind_of(PDF::Reader::BrowserPage)
     end
   end
 
   it "should return a single BrowserPage from page()" do
-    browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/no_text_spaces.pdf")
-    browser.page(1).should be_a_kind_of(PDF::Reader::BrowserPage)
+    @browser.page(1).should be_a_kind_of(PDF::Reader::BrowserPage)
   end
 end
