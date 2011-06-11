2574497b9881f04d64f25d8f1f77aae3bacba411
James Healy <jimmy@deefa.com>
2011-06-11 14:16:49 +1000

    group BrowserPage specs by method, not test file
    * easier to rapidly check all expected behaviour for a single method

--- spec/browser_page_spec.rb
+++ spec/browser_page_spec.rb
@@ -3,33 +3,52 @@
 $LOAD_PATH << "." unless $LOAD_PATH.include?(".")
 require File.dirname(__FILE__) + "/spec_helper"
 
-describe PDF::Reader::BrowserPage, "with cairo-basic.pdf" do
+describe PDF::Reader::BrowserPage, "fonts()" do
 
-  before(:each) do
+  it "should return a hash with the correct size from cairo-basic.pdf page 1" do
     @browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
     @page    = @browser.page(1)
-  end
 
-  it "should return a hash with the correct size from fonts()" do
     @page.fonts.should      be_a_kind_of(Hash)
     @page.fonts.size.should eql(1)
     @page.fonts.keys.should eql([:"CairoFont-0-0"])
   end
+end
 
-  it "should return a hash with the correct size from xobjects()" do
+describe PDF::Reader::BrowserPage, "xobjects()" do
+  it "should return a hash with the correct size from cairo-basic.pdf page 1" do
+    @browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
+    @page    = @browser.page(1)
+
     @page.xobjects.should be_a_kind_of(Hash)
     @page.xobjects.should be_empty
   end
+end
 
-  it "should return a string from raw_content()" do
+describe PDF::Reader::BrowserPage, "xobjects()" do
+  it "should return a string from raw_content() from cairo-basic.pdf page 1" do
+    @browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
+    @page    = @browser.page(1)
+
     @page.raw_content.should be_a_kind_of(String)
   end
+end
 
-  it "should return the text content of the page" do
+describe PDF::Reader::BrowserPage, "text()" do
+  it "should return the text content from cairo-basic.pdf page 1" do
+    @browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
+    @page    = @browser.page(1)
+
     @page.text.should eql("Hello James")
   end
+end
 
-  it "should run callbacks while walking a page content stream" do
+describe PDF::Reader::BrowserPage, "walk()" do
+
+  it "should run callbacks while walking content stream from cairo-basic.pdf page 1" do
+    @browser = PDF::Reader::Browser.new(File.dirname(__FILE__) + "/data/cairo-basic.pdf")
+    @page    = @browser.page(1)
+
     receiver = PDF::Reader::RegisterReceiver.new
     @page.walk(receiver)
 
