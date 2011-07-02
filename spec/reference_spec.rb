53b86055822b7f1959a4f50780dd128286f53f53
James Healy <jimmy@deefa.com>
2011-07-02 13:48:27 +1000

    spec PDF::Reader::Reference#==

--- spec/reference_spec.rb
+++ spec/reference_spec.rb
@@ -11,4 +11,28 @@ describe PDF::Reader::Reference, "hash method" do
 
     one.hash.should == two.hash
   end
+
+end
+
+describe PDF::Reader::Reference, "== method" do
+
+  it "should return true for the same object" do
+    one = PDF::Reader::Reference.new(1,0)
+
+    (one == one).should be_true
+  end
+
+  it "should return true for 2 identical objects" do
+    one = PDF::Reader::Reference.new(1,0)
+    two = PDF::Reader::Reference.new(1,0)
+
+    (one == two).should be_true
+  end
+
+  it "should return false if one object isn't a Reference" do
+    one = PDF::Reader::Reference.new(1,0)
+
+    (one == "two").should be_false
+  end
+
 end
