31a20ce2b1fcd44ef2713249d24c5097c74a5a17
James Healy <jimmy@deefa.com>
2011-07-15 00:04:44 +1000

    update 2 examples to the proposed new API

--- examples/extract_bates.rb
+++ examples/extract_bates.rb
@@ -10,26 +10,20 @@
 # the number is to look for words that match a pattern.
 #
 # This example attempts to extract numbers using the Acrobat 9 syntax.
-# As a fall back, you can provide a regular expression that will be
-# used to look for words that look like the numbers you expect in the
-# page content.
+# As a fall back, you can use a regular expression to look for words
+# that match the numbers you expect in the page content.
 
 require 'rubygems'
 require 'pdf/reader'
 
 class BatesReceiver
 
-  def initialize(regexp = nil)
-    @numbers = []
-    @backup  = []
-    @regexp  = regexp
-  end 
+  attr_reader :numbers
 
-  def numbers
-    @numbers.size > 0 ? @numbers : @backup
+  def initialize
+    @numbers = []
   end
 
-  # Called when page parsing starts
   def begin_marked_content(*args)
     return unless args.size >= 2
     return unless args.first == :Artifact
@@ -39,25 +33,17 @@ class BatesReceiver
   end
   alias :begin_marked_content_with_pl :begin_marked_content
 
-  # record text that is drawn on the page
-  def show_text(string, *params)
-    return if @regexp.nil?
-
-    string.scan(@regexp).each { |m| @backup << m }
-  end
-
-  # there's a few text callbacks, so make sure we process them all
-  alias :super_show_text :show_text
-  alias :move_to_next_line_and_show_text :show_text
-  alias :set_spacing_next_line_show_text :show_text
-
-  # this final text callback takes slightly different arguments
-  def show_text_with_positioning(*params)
-    params = params.first
-    params.each { |str| show_text(str) if str.kind_of?(String)}
-  end
 end
 
-receiver = BatesReceiver.new(/CC.+/)
-PDF::Reader.file("bates.pdf", receiver)
-puts receiver.numbers.inspect
+
+PDF::Reader.open("bates.pdf") do |reader|
+  reader.pages.each do |page|
+    receiver = BatesReceiver.new
+    page.walk(receiver)
+    if receiver.numbers.empty?
+      puts page.scan(/CC.+/)
+    else
+      puts receiver.numbers.inspect
+    end
+  end
+end
