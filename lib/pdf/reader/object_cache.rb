46bc97e7e2ea29efc14c54f8ad1b15bf7490b1d1
James Healy <jimmy@deefa.com>
2011-07-19 00:05:39 +1000

    Revert "implement a basic LFU expiration strategy in ObjectCache"
    This reverts commit 4ac4be2fbd512f57fa4040b75eb78bf8a0ec70d1.
    
    * The LFU didn't appear to make much difference in processing speed

--- lib/pdf/reader/object_cache.rb
+++ lib/pdf/reader/object_cache.rb
@@ -11,99 +11,75 @@ class PDF::Reader
     # These object types use little memory and are accessed a heap of times as
     # part of random page access, so we'll cache the unmarshalled objects and
     # avoid lots of repetitive (and expensive) tokenising
-    CORE_TYPES = [:Catalog, :Page, :Pages]
+    CACHEABLE_TYPES = [:Catalog, :Page, :Pages]
 
     def initialize
-      @core = {}
-      @lfu  = {}
-      @max_entries = 1000
-      @buffer = 50
+      @objects = {}
     end
 
     def [](key)
-      if @core.has_key?(key)
-        @core[key]
-      elsif @lfu.has_key?(key)
-        count = @lfu[key].first + 1
-        obj   = @lfu[key].last
-        @lfu[key] = [count, obj]
-        obj
-      else
-        nil
-      end
+      @objects[key]
     end
 
     def []=(key, value)
-      if core?(value)
-        @core[key] = value
-      else
-        @lfu[key] = [1, value]
-        prune
-      end
+      @objects[key] = value if cacheable?(value)
+    end
+
+    def fetch(key, local_default = nil)
+      @objects.fetch(key, local_default)
     end
 
     def each(&block)
-      @core.each(&block)
+      @objects.each(&block)
     end
     alias :each_pair :each
 
     def each_key(&block)
-      @core.each_key(&block)
+      @objects.each_key(&block)
     end
 
     def each_value(&block)
-      @core.each_value(&block)
+      @objects.each_value(&block)
     end
 
     def size
-      @core.size + @lfu.size
+      @objects.size
     end
     alias :length :size
 
     def empty?
-      @core.empty? && @lfu.empty?
+      @objects.empty?
     end
 
     def has_key?(key)
-      @core.has_key?(key) || @lfu.has_key?(key)
+      @objects.has_key?(key)
     end
     alias :include? :has_key?
     alias :key? :has_key?
     alias :member? :has_key?
 
     def has_value?(value)
-      @core.has_value?(value) || @lfu.has_value?(value)
+      @objects.has_value?(value)
     end
 
     def to_s
-      "<PDF::Reader::ObjectCache core: #{@core.size} lfu: #{@lfu.size}>"
+      "<PDF::Reader::ObjectCache size: #{self.size}>"
     end
 
     def keys
-      @core.keys + @lfu.keys
+      @objects.keys
     end
 
     def values
-      @core.values + @lfu.values
+      @objects.values
     end
 
     private
 
-    def core?(obj)
-      obj.is_a?(Hash) && CORE_TYPES.include?(obj[:Type])
+    def cacheable?(obj)
+      obj.is_a?(Hash) && CACHEABLE_TYPES.include?(obj[:Type])
     end
 
-    def prune
-      return if @lfu.size < @max_entries
-
-      lowest_count = @lfu.values.map(&:first).sort.first
-
-      @lfu.select { |key, obj|
-        obj.first == lowest_count
-      }.keys.slice(0,@buffer).each do |key|
-        @lfu.delete(key)
-      end
-    end
 
   end
 end
