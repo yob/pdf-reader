7be59a687c3ad72e791e7eb128228ac521b307d6
James Healy <jimmy@deefa.com>
2011-07-19 13:02:18 +1000

    rename browser_spec
    * what used to be PDF::Reader::Browser is now just PDF::Reader

--- spec/new_reader_spec.rb
+++ spec/new_reader_spec.rb
@@ -17,19 +17,19 @@ describe PDF::Reader, "with cairo-basic.pdf" do
 
   before(:each) do
     filename = pdf_spec_file("cairo-basic")
-    @browser = PDF::Reader.new(filename)
+    @reader = PDF::Reader.new(filename)
   end
 
   it "should return the correct pdf_version" do
-    @browser.pdf_version.should eql(1.4)
+    @reader.pdf_version.should eql(1.4)
   end
 
   it "should return the correct page_count" do
-    @browser.page_count.should eql(2)
+    @reader.page_count.should eql(2)
   end
 
   it "should return the correct info hash" do
-    info    = @browser.info
+    info    = @reader.info
 
     info.size.should eql(2)
     info[:Creator].should eql("cairo 1.4.6 (http://cairographics.org)")
@@ -37,19 +37,19 @@ describe PDF::Reader, "with cairo-basic.pdf" do
   end
 
   it "should return the nil for metadata" do
-    @browser.metadata.should be_nil
+    @reader.metadata.should be_nil
   end
 
   it "should return an array of Pages from pages()" do
-    @browser.pages.should be_a_kind_of(Array)
-    @browser.pages.size.should eql(@browser.page_count)
-    @browser.pages.each do |page|
+    @reader.pages.should be_a_kind_of(Array)
+    @reader.pages.size.should eql(@reader.page_count)
+    @reader.pages.each do |page|
       page.should be_a_kind_of(PDF::Reader::Page)
     end
   end
 
   it "should return a single Page from page()" do
-    @browser.page(1).should be_a_kind_of(PDF::Reader::Page)
+    @reader.page(1).should be_a_kind_of(PDF::Reader::Page)
   end
 
 end
@@ -58,37 +58,37 @@ describe PDF::Reader, "with no_text_spaces.pdf" do
 
   before(:each) do
     filename = pdf_spec_file("no_text_spaces")
-    @browser = PDF::Reader.new(filename)
+    @reader = PDF::Reader.new(filename)
   end
 
   it "should return the correct pdf_version" do
-    @browser.pdf_version.should eql(1.4)
+    @reader.pdf_version.should eql(1.4)
   end
 
   it "should return the correct page_count" do
-    @browser.page_count.should eql(6)
+    @reader.page_count.should eql(6)
   end
 
   it "should return the correct info hash" do
-    info    = @browser.info
+    info    = @reader.info
 
     info.size.should eql(9)
   end
 
   it "should return the nil for metadata" do
-    @browser.metadata.should be_a_kind_of(String)
-    @browser.metadata.should include("<x:xmpmeta")
+    @reader.metadata.should be_a_kind_of(String)
+    @reader.metadata.should include("<x:xmpmeta")
   end
 
   it "should return an array of Pages from pages()" do
-    @browser.pages.should be_a_kind_of(Array)
-    @browser.pages.size.should eql(@browser.page_count)
-    @browser.pages.each do |page|
+    @reader.pages.should be_a_kind_of(Array)
+    @reader.pages.size.should eql(@reader.page_count)
+    @reader.pages.each do |page|
       page.should be_a_kind_of(PDF::Reader::Page)
     end
   end
 
   it "should return a single Page from page()" do
-    @browser.page(1).should be_a_kind_of(PDF::Reader::Page)
+    @reader.page(1).should be_a_kind_of(PDF::Reader::Page)
   end
 end
