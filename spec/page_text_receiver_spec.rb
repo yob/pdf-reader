45911ab18d56a17e44c0172355156ae9e58ca173
James Healy <jimmy@deefa.com>
2011-07-24 17:28:43 +1000

    detailed testing of text extraction should be perform using PageTextReceiver class
    * testing of Page#text() should just ensure the basic case works as
      expected

---
+++ spec/page_text_receiver_spec.rb
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe PDF::Reader::PageTextReceiver do
+
+  it "should return the text content from cairo-basic.pdf page 1" do
+    @reader   = PDF::Reader.new(pdf_spec_file("cairo-basic"))
+    @page     = @reader.page(1)
+    @receiver = PDF::Reader::PageTextReceiver.new(@page.fonts)
+
+    @page.walk(@receiver)
+
+    @receiver.content.should eql("Hello James")
+  end
+
+  it "should return the text content from cairo-multiline.pdf page 1" do
+    @reader   = PDF::Reader.new(pdf_spec_file("cairo-multiline"))
+    @page     = @reader.page(1)
+    @receiver = PDF::Reader::PageTextReceiver.new(@page.fonts)
+
+    @page.walk(@receiver)
+
+    @receiver.content.should eql("Hello World\nFrom James")
+  end
+end
