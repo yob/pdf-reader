d86f1d190046989a8855d1ac230810d13c1cdad1
James Healy <jimmy@deefa.com>
2011-07-29 15:33:59 +1000

    PageTextReceiver should raise an exception when it encounters an invalid font
    * it might be better to just add the unknown char to the output, but in
      the short term I want PageTextReceiver to behave like the old API

--- lib/pdf/reader/page_text_receiver.rb
+++ lib/pdf/reader/page_text_receiver.rb
@@ -156,6 +156,7 @@ module PDF
 
       # record text that is drawn on the page
       def show_text(string) # Tj
+        raise PDF::Reader::MalformedPDFError, "current font is invalid" if current_font.nil?
         at = transform(Point.new(0,0))
         @content[at.y] ||= ""
         @content[at.y] << current_font.to_utf8(string)
