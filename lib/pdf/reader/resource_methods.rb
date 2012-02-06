a00519b8e01934208bf51557783023276086fc38
James Healy <jimmy@deefa.com>
2012-02-06 19:28:25 +1100

    memoize page resources
    * this will improve performance significantly 9at the expense of memory)
      for Pages with a large number of resources
    * the long term solution is to improve the ObjectCache to cache commonly
      accessed objects

--- lib/pdf/reader/resource_methods.rb
+++ lib/pdf/reader/resource_methods.rb
@@ -9,50 +9,50 @@ module PDF
       # Returns a Hash of color spaces that are available to this page
       #
       def color_spaces
-        @objects.deref!(resources[:ColorSpace]) || {}
+        @color_spaces ||= @objects.deref!(resources[:ColorSpace]) || {}
       end
 
       # Returns a Hash of fonts that are available to this page
       #
       def fonts
-        @objects.deref!(resources[:Font]) || {}
+        @graphic_states ||= @objects.deref!(resources[:Font]) || {}
       end
 
       # Returns a Hash of external graphic states that are available to this
       # page
       #
       def graphic_states
-        @objects.deref!(resources[:ExtGState]) || {}
+        @graphic_states ||= @objects.deref!(resources[:ExtGState]) || {}
       end
 
       # Returns a Hash of patterns that are available to this page
       #
       def patterns
-        @objects.deref!(resources[:Pattern]) || {}
+        @patterns ||= @objects.deref!(resources[:Pattern]) || {}
       end
 
       # Returns an Array of procedure sets that are available to this page
       #
       def procedure_sets
-        @objects.deref!(resources[:ProcSet]) || []
+        @procedure_sets ||= @objects.deref!(resources[:ProcSet]) || []
       end
 
       # Returns a Hash of properties sets that are available to this page
       #
       def properties
-        @objects.deref!(resources[:Properties]) || {}
+        @properties ||= @objects.deref!(resources[:Properties]) || {}
       end
 
       # Returns a Hash of shadings that are available to this page
       #
       def shadings
-        @objects.deref!(resources[:Shading]) || {}
+        @shadings ||= @objects.deref!(resources[:Shading]) || {}
       end
 
       # Returns a Hash of XObjects that are available to this page
       #
       def xobjects
-        @objects.deref!(resources[:XObject]) || {}
+        @xobjects ||= @objects.deref!(resources[:XObject]) || {}
       end
 
     end
