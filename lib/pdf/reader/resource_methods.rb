6f90341ffe3a2a06b44460e5d01c9af373b3a3bd
James Healy <jimmy@deefa.com>
2012-02-07 14:23:33 +1100

    Revert "memoize page resources"
    This reverts commit a00519b8e01934208bf51557783023276086fc38.

--- lib/pdf/reader/resource_methods.rb
+++ lib/pdf/reader/resource_methods.rb
@@ -9,50 +9,50 @@ module PDF
       # Returns a Hash of color spaces that are available to this page
       #
       def color_spaces
-        @color_spaces ||= @objects.deref!(resources[:ColorSpace]) || {}
+        @objects.deref!(resources[:ColorSpace]) || {}
       end
 
       # Returns a Hash of fonts that are available to this page
       #
       def fonts
-        @graphic_states ||= @objects.deref!(resources[:Font]) || {}
+        @objects.deref!(resources[:Font]) || {}
       end
 
       # Returns a Hash of external graphic states that are available to this
       # page
       #
       def graphic_states
-        @graphic_states ||= @objects.deref!(resources[:ExtGState]) || {}
+        @objects.deref!(resources[:ExtGState]) || {}
       end
 
       # Returns a Hash of patterns that are available to this page
       #
       def patterns
-        @patterns ||= @objects.deref!(resources[:Pattern]) || {}
+        @objects.deref!(resources[:Pattern]) || {}
       end
 
       # Returns an Array of procedure sets that are available to this page
       #
       def procedure_sets
-        @procedure_sets ||= @objects.deref!(resources[:ProcSet]) || []
+        @objects.deref!(resources[:ProcSet]) || []
       end
 
       # Returns a Hash of properties sets that are available to this page
       #
       def properties
-        @properties ||= @objects.deref!(resources[:Properties]) || {}
+        @objects.deref!(resources[:Properties]) || {}
       end
 
       # Returns a Hash of shadings that are available to this page
       #
       def shadings
-        @shadings ||= @objects.deref!(resources[:Shading]) || {}
+        @objects.deref!(resources[:Shading]) || {}
       end
 
       # Returns a Hash of XObjects that are available to this page
       #
       def xobjects
-        @xobjects ||= @objects.deref!(resources[:XObject]) || {}
+        @objects.deref!(resources[:XObject]) || {}
       end
 
     end
