b55ecf2bd61ba66e61dd9a3a49c0ffe6c40d344c
James Healy <jimmy@deefa.com>
2012-05-23 02:02:57 +1000

    save a copy of a debugging script I've been using

---
+++ tools/bench.rb
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+# a quick script for profiling performance with perftools.
+#
+# USAGE
+#
+#     ruby tools/bench.rb
+#     evince bench.pdf
+
+$:.unshift "../lib"
+require 'pdf-reader'
+require 'perftools'
+
+PerfTools::CpuProfiler.start("/tmp/bench.tmp") do
+  PDF::Reader.open("restart.pdf") do |reader|
+    reader.pages.each do |page|
+      page.text
+    end
+  end
+end
+
+`pprof.rb --text /tmp/restart_profile > bench.txt`
+`pprof.rb --pdf  /tmp/restart_profile > bench.pdf`
