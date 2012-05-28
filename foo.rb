b76fba368f211785c9f2472f87741e48cad61cde
James Healy <jimmy@deefa.com>
2012-05-28 18:09:45 +1000

    get specs green with new PDF grammer

--- foo.rb
+++ foo.rb
@@ -3,76 +3,67 @@ require 'rspec'
 
 module PDF
   class Reader
-    class Token < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [self.text_value]
-      end
-    end
-
     class ArrayNode < Treetop::Runtime::SyntaxNode
-      def to_ary
-        elements[1].elements.flatten.select { |obj|
-          !obj.is_a?(Treetop::Runtime::SyntaxNode)
-        }
+      def to_ruby
+        elements[1].elements.select { |obj|
+          obj.respond_to?(:to_ruby)
+        }.map(&:to_ruby)
       end
     end
 
     class DictNode < Treetop::Runtime::SyntaxNode
-      def to_ary
-        ret = elements[1].elements.flatten.select { |obj|
-          !obj.is_a?(Treetop::Runtime::SyntaxNode)
+      def to_ruby
+        ret = {}
+        entries = elements[1].elements.map(&:elements).flatten.select { |node|
+          node.elements.any? { |e| e.respond_to?(:to_ruby)}
         }
-        [ Hash[*ret] ]
+        entries.each do |entry|
+          interesting_nodes = entry.elements.select { |e| e.respond_to?(:to_ruby) }
+          ret[interesting_nodes.first.to_ruby] = interesting_nodes.last.to_ruby
+        end
+        ret
       end
     end
 
     class Integer < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [ text_value.to_i ]
+      def to_ruby
+        text_value.to_i
       end
     end
 
     class Float < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [ text_value.to_f ]
+      def to_ruby
+        text_value.to_f
       end
     end
 
     class Name < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [ elements[1].text_value.to_sym ]
+      def to_ruby
+        elements[1].text_value.to_sym
       end
     end
 
-    class BooleanTrue < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [ true ]
-      end
-    end
-
-    class BooleanFalse < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [ false ]
+    class BooleanNode < Treetop::Runtime::SyntaxNode
+      def to_ruby
+        text_value == "true"
       end
     end
 
     class NullNode < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [ nil ]
+      def to_ruby
+        nil
       end
     end
 
     class HexString < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [
-          elements[1].text_value.scan(/../).map { |i| i.hex.chr }.join
-        ]
+      def to_ruby
+        elements[1].text_value.scan(/../).map { |i| i.hex.chr }.join
       end
     end
 
     class LiteralString < Treetop::Runtime::SyntaxNode
-      def to_ary
-        [elements[1].text_value]
+      def to_ruby
+        elements[1].text_value
       end
     end
   end
@@ -84,7 +75,7 @@ class Parser
 
   def self.parse(data)
     # Pass the data over to the parser instance
-    tree = @@parser.parse(data)
+    tree = @@parser.parse(data, root: :body)
 
     # If the AST is nil then there was an error during parsing
     # we need to report a simple error message to help the user
@@ -92,9 +83,9 @@ class Parser
       raise Exception, "Parse error at offset: #{@@parser.index}"
     end
 
-    tree.elements.flatten.select { |obj|
-      !obj.is_a?(Treetop::Runtime::SyntaxNode)
-    }
+    tree.elements.select { |obj|
+      obj.respond_to?(:to_ruby)
+    }.map(&:to_ruby)
   end
 
 end
@@ -198,12 +189,18 @@ describe Parser do
 
   it "should parse an array of ints" do
     str    = "[ 1 2 3 4 ]"
-    tokens = [ 1, 2, 3, 4 ]
+    tokens = [[ 1, 2, 3, 4 ]]
     Parser.parse(str).should == tokens
   end
 
   it "should parse a simple dictionary" do
     str    = "<</One 1 /Two 2>>"
+    tokens = [ {:One => 1, :Two => 2} ]
+    Parser.parse(str).should == tokens
+  end
+
+  it "should parse a simple dictionary without space between value and key" do
+    str    = "<</One 1/Two 2>>"
     tokens = [ {:One => 1, :Two => 2} ]
     Parser.parse(str).should == tokens
   end
