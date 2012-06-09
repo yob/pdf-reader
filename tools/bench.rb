dedd82d67a59a9ef6c7a547124404b8e7533a640
James Healy <jimmy@deefa.com>
2012-06-09 22:51:15 +1000

    foo

--- tools/bench.rb
+++ tools/bench.rb
@@ -7,17 +7,20 @@
 #     ruby tools/bench.rb
 #     evince bench.pdf
 
-$:.unshift "../lib"
-require 'pdf-reader'
+require 'pdf/reader'
 require 'perftools'
 
-PerfTools::CpuProfiler.start("/tmp/restart_profile") do
+# ensure we've loaded the correct pdf-reader
+PDF::Reader::NewParser
+
+#PerfTools::CpuProfiler.start("/tmp/restart_profile") do
   PDF::Reader.open("restart.pdf") do |reader|
     reader.pages.each do |page|
+      puts page.number
       page.text
     end
   end
-end
+#end
 
-`pprof.rb --text /tmp/restart_profile > bench.txt`
-`pprof.rb --pdf  /tmp/restart_profile > bench.pdf`
+#`pprof.rb --text /tmp/restart_profile > bench.txt`
+#`pprof.rb --pdf  /tmp/restart_profile > bench.pdf`
