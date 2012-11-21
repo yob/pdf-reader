e9838a62773e0eccf0737137da3f8774e4c13251
James Healy <jimmy@deefa.com>
2012-11-21 20:13:16 +0100

    break PageLayout#to_s into smaller methods
    * more to come

--- lib/pdf/reader/page_layout.rb
+++ lib/pdf/reader/page_layout.rb
@@ -6,26 +6,20 @@ class PDF::Reader
   # string that best approximates the way they'd appear on a render PDF page.
   class PageLayout
     def initialize(runs, options = {})
-      @runs    = runs
+      @runs    = merge_runs(runs)
       @options = options
       @current_platform_is_rbx_19 = RUBY_DESCRIPTION =~ /\Arubinius 2.0.0/ &&
                                       RUBY_VERSION >= "1.9.0"
     end
 
     def to_s
-      runs = @runs.group_by { |char|
-        char.y.to_i
-      }.map { |y, chars|
-        group_chars_into_runs(chars.sort)
-      }.flatten.sort
-
       row_count = @options.fetch(:number_of_rows, 100)
       col_count = @options.fetch(:number_of_cols, 200)
       row_multiplier = @options.fetch(:row_scale, 8.0) # 800
       col_multiplier = @options.fetch(:col_scale, 3.0) # 600
-      x_offset = runs.map(&:x).sort.first
+      x_offset = @runs.map(&:x).sort.first
       page = row_count.times.map { |i| " " * col_count }
-      runs.each do |run|
+      @runs.each do |run|
         x_pos = ((run.x - x_offset) / col_multiplier).round
         y_pos = row_count - (run.y / row_multiplier).round
         if y_pos < row_count && y_pos >= 0 && x_pos < col_count && x_pos >= 0
@@ -39,6 +33,16 @@ class PDF::Reader
     end
 
     private
+
+    # take a collection of TextRun objects and merge any that are in close
+    # proximity
+    def merge_runs(runs)
+      runs.group_by { |char|
+        char.y.to_i
+      }.map { |y, chars|
+        group_chars_into_runs(chars.sort)
+      }.flatten.sort
+    end
 
     def group_chars_into_runs(chars)
       runs = []
