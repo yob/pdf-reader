0a1eb1b2257ba06faa3e931d4927a33fd944bf23
James Healy <jimmy@deefa.com>
2012-11-25 19:44:25 +0100

    split column integration specs into smaller chunks

--- spec/column_spec.rb
+++ spec/column_spec.rb
@@ -4,102 +4,127 @@ require File.dirname(__FILE__) + "/spec_helper"
 
 describe PDF::Reader, "column specs" do
 
-  it "should correctly read page 1" do
-    filename = pdf_spec_file("column_integration")
+  context "page 1" do
+    it "should correctly extract the headline" do
+      filename = pdf_spec_file("column_integration")
 
-    PDF::Reader.open(filename) do |reader|
-      page = reader.page(1)
-      # the first page has one centered header
-      # followed by full width text
-      # then followed by two column text, below is the last of the one column
-      # text followed by the two column text
-      col_text = Regexp::new(<<-TEXT, Regexp::MULTILINE)
-\s*esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-\s*officia deserunt mollit anim id est laborum.
-\s*Lorem ipsum dolor sit amet, consectetur adipisic-\s{20,80}adipisicing elit, sed do eiusmod tempor incididunt
-\s*ing elit, sed do eiusmod tempor incididunt ut labore\s{20,80}ut labore et dolore magna aliqua. Ut enim ad minim
-\s*et dolore magna aliqua. Ut enim ad minim veniam,\s{20,80}veniam, quis nostrud exercitation ullamco laboris
-\s*quis nostrud exercitation ullamco laboris nisi ut\s{20,80}nisi ut aliquip ex ea commodo consequat. Duis aute
-\s*aliquip ex ea commodo consequat. Duis aute irure\s{20,80}irure dolor in reprehenderit in voluptate velit esse
-      TEXT
-      ft = page.text
-      ft.should =~ /Some Headline/
-      ft.should =~ /ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu/
-      ft.should =~ col_text
+      PDF::Reader.open(filename) do |reader|
+        page = reader.page(1)
+        page.text.should =~ /Some Headline/
+      end
+    end
+    it "should correctly extract the first few lines" do
+      filename = pdf_spec_file("column_integration")
 
-      # The following lines are in the second column, and their position with in the
-      # string (from the left) should all be at the same spot
-      match_pos_1 = find_position_of_match(ft, /\s{10}adipisicing elit, sed do eiusmod tempor incididunt$/)
-      match_pos_2 = find_position_of_match(ft, /\s{10}ut labore et dolore magna aliqua. Ut enim ad minim$/)
-      match_pos_3 = find_position_of_match(ft, /\s{10}veniam, quis nostrud exercitation ullamco laboris$/)
-      match_pos_4 = find_position_of_match(ft, /\s{10}nisi ut aliquip ex ea commodo consequat. Duis aute$/)
-      match_pos_5 = find_position_of_match(ft, /\s{10}irure dolor in reprehenderit in voluptate velit esse$/)
+      PDF::Reader.open(filename) do |reader|
+        page = reader.page(1)
+        ft = page.text
+        ft.should =~ /ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu/
+        ft.should =~ /Lorem ipsum dolor sit amet, consectetur adipisic -\s+adipisicing elit, sed do eiusmod tempor incididunt/
+        ft.should =~ /ing elit, sed do eiusmod tempor incididunt ut labore\s+ut labore et dolore magna aliqua. Ut enim ad minim/
+        ft.should =~ /et dolore magna aliqua. Ut enim ad minim veniam,\s+veniam, quis nostrud exercitation ullamco laboris/
+        ft.should =~ /quis nostrud exercitation ullamco laboris nisi ut\s+nisi ut aliquip ex ea commodo consequat. Duis aute/
+        ft.should =~ /aliquip ex ea commodo consequat. Duis aute irure\s+irure dolor in reprehenderit in voluptate velit esse/
+      end
+    end
 
-      match_pos_1.should eql(match_pos_2)
-      match_pos_1.should eql(match_pos_3)
-      match_pos_1.should eql(match_pos_4)
-      match_pos_1.should eql(match_pos_5)
+    it "should align text from the second column" do
+      filename = pdf_spec_file("column_integration")
+
+      PDF::Reader.open(filename) do |reader|
+        page = reader.page(1)
+        ft = page.text
+        # The following lines are in the second column, and their position with in the
+        # string (from the left) should all be at the same spot
+        match_pos_1 = find_position_of_match(ft, /adipisicing elit, sed do eiusmod tempor incididunt$/)
+        match_pos_2 = find_position_of_match(ft, /ut labore et dolore magna aliqua. Ut enim ad minim$/)
+        match_pos_3 = find_position_of_match(ft, /veniam, quis nostrud exercitation ullamco laboris$/)
+        match_pos_4 = find_position_of_match(ft, /nisi ut aliquip ex ea commodo consequat. Duis aute$/)
+        match_pos_5 = find_position_of_match(ft, /irure dolor in reprehenderit in voluptate velit esse$/)
+
+        match_pos_1.should_not be_nil
+        match_pos_1.should eql(match_pos_2)
+        match_pos_1.should eql(match_pos_3)
+        match_pos_1.should eql(match_pos_4)
+        match_pos_1.should eql(match_pos_5)
+      end
     end
   end
 
-  it "should correctly read page 2" do
-    filename = pdf_spec_file("column_integration")
+  context "page 2" do
+    it "should correctly align text in column 1" do
+      filename = pdf_spec_file("column_integration")
 
-    PDF::Reader.open(filename) do |reader|
-      page = reader.page(2)
-      # the second page has three columns of text with a
-      # black rectangle in the middle of the page interrupting the entire
-      # second column
-      three_to_two_cols = Regexp::new(<<-TEXT, Regexp::MULTILINE)
-\s*fugiat nulla pariatur. Excepteur\s{20,40}sunt in culpa qui officia deserunt\s{20,40}sum dolor sit amet, consectetur
-\s*sint occaecat cupidatat non proi-\s{20,40}mollit anim id est laborum. Lo-\s{20,40}adipisicing elit, sed do eiusmod
-\s*dent, sunt in culpa qui officia de-\s{20,40}rem ipsum dolor sit amet, con-\s{20,40}tempor incididunt ut labore et
-\s*serunt mollit anim id est\s{100,120}dolore magna aliqua. Ut
-\s*laborum. Lorem ipsum\s{100,120}enim ad minim veniam,
-\s*dolor sit amet, consecte-\s{100,120}quis nostrud exercitation
-      TEXT
-      two_to_three_cols = Regexp::new(<<-TEXT2, Regexp::MULTILINE)
-\s*pariatur. Excepteur sint\s{100,120}lor sit amet, consectetur
-\s*occaecat cupidatat non\s{100,120}adipisicing elit, sed do
-\s*proident, sunt in culpa\s{100,120}eiusmod tempor incidid-
-\s*qui officia deserunt mollit anim\s{20,40}sectetur adipisicing elit, sed do\s{20,40}unt ut labore et dolore magna ali-
-\s*id est laborum. Lorem ipsum do-\s{20,40}eiusmod tempor incididunt ut\s{20,40}qua. Ut enim ad minim veniam,
-\s*lor sit amet, consectetur adipisic-\s{20,40}labore et dolore magna aliqua. Ut\s{20,40}quis nostrud exercitation ullamco
-      TEXT2
-      ft = page.text
-      ft.should =~ three_to_two_cols
-      ft.should =~ two_to_three_cols
+      PDF::Reader.open(filename) do |reader|
+        ft = reader.page(2).text
 
-      # The following lines are in the second column of the page prior to the interruption
-      col2_1   = find_position_of_match(ft, /\s{10}occaecat cupidatat non proident,\s{10}/)
-      col2_2   = find_position_of_match(ft, /\s{10}sunt in culpa qui officia deserunt\s{10}/)
-      col2_3   = find_position_of_match(ft, /\s{10}mollit anim id est laborum. Lo-\s{10}/)
-      col2_4   = find_position_of_match(ft, /\s{10}rem ipsum dolor sit amet, con-\s{10}/)
+        # The following lines are in the first column of the page prior to the interruption
+        col1_1   = find_position_of_match(ft, /^tate velit esse cillum dolore eu/)
+        col1_2   = find_position_of_match(ft, /^fugiat nulla pariatur. Excepteur/)
+        col1_3   = find_position_of_match(ft, /^sint occaecat cupidatat non proi -/)
+        col1_4   = find_position_of_match(ft, /^dent, sunt in culpa qui officia de-/)
 
-      # The following lines are in the third column of the page prior to the interruption
-      col3_a_1 = find_position_of_match(ft, /\s{10}anim id est laborum. Lorem ip-$/)
-      col3_a_2 = find_position_of_match(ft, /\s{10}sum dolor sit amet, consectetur$/)
-      col3_a_3 = find_position_of_match(ft, /\s{10}adipisicing elit, sed do eiusmod$/)
-      col3_a_4 = find_position_of_match(ft, /\s{10}tempor incididunt ut labore et$/)
+        col1_1.should_not be_nil
+        col1_1.should eql(col1_2)
+        col1_1.should eql(col1_3)
+        col1_1.should eql(col1_4)
+      end
+    end
+    it "should correctly align text in column 2" do
+      filename = pdf_spec_file("column_integration")
 
-      #the following lines are in the third column of the page _during_ the interruption
-      col3_b_1 = find_position_of_match(ft, /\s{10}dolore magna aliqua. Ut$/)
-      col3_b_2 = find_position_of_match(ft, /\s{10}enim ad minim veniam,$/)
-      col3_b_3 = find_position_of_match(ft, /\s{10}quis nostrud exercitation$/)
-      col3_b_4 = find_position_of_match(ft, /\s{10}ullamco laboris nisi ut$/)
+      PDF::Reader.open(filename) do |reader|
+        ft = reader.page(2).text
 
-      col2_1.should eql(col2_2)
-      col2_1.should eql(col2_3)
-      col2_1.should eql(col2_4)
+        # The following lines are in the second column of the page prior to the interruption
+        col2_1   = find_position_of_match(ft, /occaecat cupidatat non proident,\s*anim/)
+        col2_2   = find_position_of_match(ft, /sunt in culpa qui officia deserunt\s*sum/)
+        col2_3   = find_position_of_match(ft, /mollit anim id est laborum. Lo -\s*adipisicing/)
+        col2_4   = find_position_of_match(ft, /rem ipsum dolor sit amet, con -\s*tempor/)
 
-      col3_a_1.should eql(col3_a_2)
-      col3_a_1.should eql(col3_a_3)
-      col3_a_1.should eql(col3_a_4)
+        col2_1.should_not be_nil
+        col2_1.should eql(col2_2)
+        col2_1.should eql(col2_3)
+        col2_1.should eql(col2_4)
+      end
+    end
 
-      col3_b_1.should eql(col3_b_2)
-      col3_b_1.should eql(col3_b_3)
-      col3_b_1.should eql(col3_b_4)
+    it "should correctly align text in column 3 before the interruption" do
+      filename = pdf_spec_file("column_integration")
 
+      PDF::Reader.open(filename) do |reader|
+        ft = reader.page(2).text
+
+        # The following lines are in the third column of the page prior to the interruption
+        col3_a_1 = find_position_of_match(ft, /anim id est laborum. Lorem ip -$/)
+        col3_a_2 = find_position_of_match(ft, /sum dolor sit amet, consectetur$/)
+        col3_a_3 = find_position_of_match(ft, /adipisicing elit, sed do eiusmod$/)
+        col3_a_4 = find_position_of_match(ft, /tempor incididunt ut labore et$/)
+
+        col3_a_1.should_not be_nil
+        col3_a_1.should eql(col3_a_2)
+        col3_a_1.should eql(col3_a_3)
+        col3_a_1.should eql(col3_a_4)
+      end
+    end
+
+    it "should correctly align text in column 3 during the interruption" do
+      filename = pdf_spec_file("column_integration")
+
+      PDF::Reader.open(filename) do |reader|
+        ft = reader.page(2).text
+
+        #the following lines are in the third column of the page _during_ the interruption
+        col3_b_1 = find_position_of_match(ft, /\s{10}dolore magna aliqua. Ut$/)
+        col3_b_2 = find_position_of_match(ft, /\s{10}enim ad minim veniam,$/)
+        col3_b_3 = find_position_of_match(ft, /\s{10}quis nostrud exercitation$/)
+        col3_b_4 = find_position_of_match(ft, /\s{10}ullamco laboris nisi ut$/)
+
+        col3_b_1.should_not be_nil
+        col3_b_1.should eql(col3_b_2)
+        col3_b_1.should eql(col3_b_3)
+        col3_b_1.should eql(col3_b_4)
+      end
     end
   end
 
@@ -109,6 +134,7 @@ describe PDF::Reader, "column specs" do
         return x_pos
       end
     end
+    nil
   end
 
 end
