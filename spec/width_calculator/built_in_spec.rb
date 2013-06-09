5ed1a58763bdc6a2093d7bce8288ab2b760d1952
Alan deLevie <adelevie@gmail.com>
2013-06-09 13:29:48 -0400

    created UnknownGlyphWidthError with test

--- spec/width_calculator/built_in_spec.rb
+++ spec/width_calculator/built_in_spec.rb
@@ -9,6 +9,20 @@ describe PDF::Reader::WidthCalculator::BuiltIn do
   end
 end
 
+describe PDF::Reader::WidthCalculator::BuiltIn, "#glyph_width" do
+  it "should raise an UnknownGlyphWidthError when the glyph width is unknown" do
+    lambda { 
+      file = File.new(pdf_spec_file("unknown_glyph_width_sample"))
+      pdf = PDF::Reader.new(file)
+      
+      # this is a three page PDF.
+      # only the second page contains the unknown glypth width
+      pdf.pages[1].text        
+
+    }.should raise_error(PDF::Reader::UnknownGlyphWidthError)
+  end
+end
+
 describe PDF::Reader::WidthCalculator::BuiltIn, "#initialize" do
   context "when the basefont is one of the 14 standard fonts" do
     let!(:font)        { double(:basefont => :Helvetica) }
