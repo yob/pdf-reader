3fd56de67e41f2082f1cf2a05603b9deaec1d508
James Healy <james@yob.id.au>
2017-02-07 22:34:44 +1100

    appease cane

--- spec/reader/filter/flate_spec.rb
+++ spec/reader/filter/flate_spec.rb
@@ -4,20 +4,42 @@ describe PDF::Reader::Filter::Flate do
   describe "#filter" do
     it "inflates a RFC1950 (zlib) deflated stream correctly"
     it "inflates a raw RFC1951 deflated stream correctly"
-    it "inflates a deflated stream with PNG predictors correctly" do
-      filter = PDF::Reader::Filter::Flate.new(:Columns => 5, :Predictor => 12)
-      deflated_data    = binread(File.dirname(__FILE__) + "/../../data/deflated_with_predictors.dat")
-      depredicted_data = binread(File.dirname(__FILE__) + "/../../data/deflated_with_predictors_result.dat")
-      expect(filter.filter(deflated_data)).to eql(depredicted_data)
+
+    context "deflated stream with PNG predictors" do
+      let(:deflated_path) {
+        File.dirname(__FILE__) + "/../../data/deflated_with_predictors.dat"
+      }
+      let(:depredicted_path) {
+        File.dirname(__FILE__) + "/../../data/deflated_with_predictors_result.dat"
+      }
+      let(:deflated_data) { binread(deflated_path) }
+      let(:depredicted_data) { binread(depredicted_path) }
+
+      it "inflates the data" do
+        filter = PDF::Reader::Filter::Flate.new(
+          :Columns => 5,
+          :Predictor => 12
+        )
+        expect(filter.filter(deflated_data)).to eql(depredicted_data)
+      end
     end
 
-    it "inflates a deflated stream with tiff predictors correctly" do
-      filter         = PDF::Reader::Filter::Flate.new(:Columns => 5, :Predictor => 2, :Colors => 3)
-      original_data  = "abcabcabcabcabcabcabcabcabcabc"
-      predicted_data = "abc\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00abc\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-      deflated_data  = Zlib::Deflate.deflate(predicted_data)
+    context "deflated stream with tiff predictors" do
+      let(:original_data) { "abcabcabcabcabcabcabcabcabcabc" }
+      let(:predicted_data)  {
+        "abc\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00abc\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+      }
 
-      expect(filter.filter(deflated_data)).to eql(original_data)
+      it "inflates the data" do
+        filter = PDF::Reader::Filter::Flate.new(
+          :Columns => 5,
+          :Predictor => 2,
+          :Colors => 3
+        )
+        deflated_data = Zlib::Deflate.deflate(predicted_data)
+
+        expect(filter.filter(deflated_data)).to eql(original_data)
+      end
     end
   end
 end
