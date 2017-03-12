17d3240883a5e14fe175115609daad097eeddb0e
Gyuchang Jun <gyuchang@point.com>
2017-03-11 18:11:33 -0800

    split `StandardSecurityHandler#decrypt` into two smaller, private methods

--- lib/pdf/reader/standard_security_handler.rb
+++ lib/pdf/reader/standard_security_handler.rb
@@ -80,31 +80,47 @@ class PDF::Reader
     #
     # Algorithm 1: Encryption of data using the RC4 or AES algorithms
     #
-    # used to decrypt RC4 encrypted PDF streams (buf)
+    # used to decrypt RC4/AES encrypted PDF streams (buf)
     #
     # buf - a string to decrypt
     # ref - a PDF::Reader::Reference for the object to decrypt
     #
     def decrypt( buf, ref )
-      objKey = @encrypt_key.dup
-      (0..2).each { |e| objKey << (ref.id >> e*8 & 0xFF ) }
-      (0..1).each { |e| objKey << (ref.gen >> e*8 & 0xFF ) }
-      if @cfm == :AESV2
-        objKey << 'sAlT'  # Algorithm 1, b)
-        length = objKey.length < 16 ? objKey.length : 16
-        cipher = OpenSSL::Cipher.new("AES-#{length << 3}-CBC")
-        cipher.decrypt
-        cipher.key = Digest::MD5.digest(objKey)[0,length]
-        cipher.iv = buf[0..15]
-        cipher.update(buf[16..-1]) + cipher.final
-      else
-        length = objKey.length < 16 ? objKey.length : 16
-        rc4 = RC4.new( Digest::MD5.digest(objKey)[0,length] )
-        rc4.decrypt(buf)
+      case @cfm
+        when :AESV2
+          decrypt_aes128(buf, ref)
+        else
+          decrypt_rc4(buf, ref)
       end
     end
 
     private
+
+    # decrypt with RC4 algorithm
+    # version <=3 or (version == 4 and CFM == V2)
+    def decrypt_rc4( buf, ref )
+      objKey = @encrypt_key.dup
+      (0..2).each { |e| objKey << (ref.id >> e*8 & 0xFF ) }
+      (0..1).each { |e| objKey << (ref.gen >> e*8 & 0xFF ) }
+      length = objKey.length < 16 ? objKey.length : 16
+      rc4 = RC4.new( Digest::MD5.digest(objKey)[0,length] )
+      rc4.decrypt(buf)
+    end
+
+    # decrypt with AES-128-CBC algorithm
+    # when (version == 4 and CFM == AESV2)
+    def decrypt_aes128( buf, ref )
+      objKey = @encrypt_key.dup
+      (0..2).each { |e| objKey << (ref.id >> e*8 & 0xFF ) }
+      (0..1).each { |e| objKey << (ref.gen >> e*8 & 0xFF ) }
+      objKey << 'sAlT'  # Algorithm 1, b)
+      length = objKey.length < 16 ? objKey.length : 16
+      cipher = OpenSSL::Cipher.new("AES-#{length << 3}-CBC")
+      cipher.decrypt
+      cipher.key = Digest::MD5.digest(objKey)[0,length]
+      cipher.iv = buf[0..15]
+      cipher.update(buf[16..-1]) + cipher.final
+    end
 
     # Pads supplied password to 32bytes using PassPadBytes as specified on
     # pp61 of spec
