2cea4129b9d3d298b0dd7fcafc4a9f58efac18e5
James Healy <james@yob.id.au>
2019-12-05 00:50:51 +1100

    fix long line

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1143,7 +1143,8 @@ describe PDF::Reader, "integration specs" do
   context "PDF with page rotation followed by matrix transformations to undo it" do
     let(:filename) { pdf_spec_file("rotate-then-undo") }
     let(:text) {
-      "This page uses matrix transformations to print text sideways, then has a Rotate key to fix it"
+      "This page uses matrix transformations to print text sideways, " +
+      "then has a Rotate key to fix it"
     }
 
     it "extracts text correctly" do
