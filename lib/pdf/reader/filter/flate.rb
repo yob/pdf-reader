546dd5d042461f2d280c7e1c034e823d2ca82722
James Healy <james@yob.id.au>
2020-06-14 21:19:01 +1000

    update stale comment
    * use correct RFC number
    * remove dead link

--- lib/pdf/reader/filter/flate.rb
+++ lib/pdf/reader/filter/flate.rb
@@ -20,13 +20,12 @@ class PDF::Reader
           deflated = Zlib::Inflate.new.inflate(data)
         rescue Zlib::DataError => e
           # by default, Ruby's Zlib assumes the data it's inflating
-          # is RFC1951 deflated data, wrapped in a RFC1951 zlib container.
-          # If that fails, then use an undocumented 'feature' to attempt to inflate
+          # is RFC1951 deflated data, wrapped in a RFC1950 zlib container. If that
+          # fails, then use a lightly-documented 'feature' to attempt to inflate
           # the data as a raw RFC1951 stream.
           #
           # See
           # - http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/243545
-          # - http://www.gzip.org/zlib/zlib_faq.html#faq38
           deflated = Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(data)
         end
         Depredict.new(@options).filter(deflated)
