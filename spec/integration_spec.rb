f3d0ad4160ab6a885620c19be8857291f6106759
James Healy <james@yob.id.au>
2022-01-11 23:50:04 +1100

    move integration specs for invalid files into a dedicated file
    spec/integration_spec.rb is getting kind a long, and I'm preparing to
    add more specs that specifically test broken files. I think it's worth
    moving these to a dedicated spot.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -196,20 +196,6 @@ describe PDF::Reader, "integration specs" do
     end
   end
 
-  context "PDF that has a content stream with a broken string" do
-    let(:filename) { pdf_spec_file("broken_string") }
-
-    # this file used to get us into a hard, endless loop. Make sure that doesn't still happen
-    it "doesn't hang when extracting doc info" do
-      Timeout::timeout(3) do
-        expect {
-          reader = PDF::Reader.new(filename)
-          reader.info
-        }.to raise_error(PDF::Reader::MalformedPDFError)
-      end
-    end
-  end
-
   context "PDF with a stream that has its length specified as an indirect reference" do
     let(:filename) { pdf_spec_file("content_stream_with_length_as_ref") }
 
@@ -230,25 +216,6 @@ describe PDF::Reader, "integration specs" do
       PDF::Reader.open(filename) do |reader|
         expect(reader.page(1).text).to eql("Hello World")
       end
-    end
-  end
-
-  context "PDF has a content stream refers to a non-existant font" do
-    let(:filename) { pdf_spec_file("content_stream_refers_to_invalid_font") }
-
-    it "raises an exception" do
-      expect {
-        reader = PDF::Reader.new(filename)
-        reader.page(1).text
-      }.to raise_error(PDF::Reader::MalformedPDFError)
-    end
-  end
-
-  context "Empty file" do
-    it "raises an exception" do
-      expect {
-        PDF::Reader.new(StringIO.new(""))
-      }.to raise_error(PDF::Reader::MalformedPDFError)
     end
   end
 
@@ -1336,26 +1303,6 @@ describe PDF::Reader, "integration specs" do
       PDF::Reader.open(filename) do |reader|
         page = reader.page(1)
         expect(page.text).to eq("aaaabbbb")
-      end
-    end
-  end
-
-  context "Malformed PDF" do
-    let(:filename) { pdf_spec_file("trailer_root_is_not_a_dict") }
-
-    it "raises an exception if trailer Root is not a dict" do
-      PDF::Reader.open(filename) do |reader|
-        expect { reader.page(1) }.to raise_error(PDF::Reader::MalformedPDFError)
-      end
-    end
-  end
-
-  context "PDF with missing page data" do
-    let(:filename) { pdf_spec_file("invalid_pages") }
-
-    it "raises a MalformedPDFError when an InvalidPageError is raised internally" do
-      PDF::Reader.open(filename) do |reader|
-        expect { reader.pages }.to raise_error(PDF::Reader::MalformedPDFError)
       end
     end
   end
